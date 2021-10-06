aae541ccf93b4a0cbc265bdd69f1b4f5c23bb04f
Luan Ferreira Campos <86481756+luanfcampos@users.noreply.github.com>
2021-10-06 20:44:44 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import * as C from './App.styles'
+import { Item } from './types/item'
+import { Category } from './types/category'
+import { categories } from './data/categories'
+import { items } from './data/items'
+import { getCurrentMonth, filterListByMonth } from './helpers/dateFilter'
+import { TableArea } from './components/TableArea'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [list, setList] = useState(items)
+  const [filteredList, setFilteredList] = useState<Item[ ]>([])
+  const [currentMonth, setCurrenteMonth] = useState(getCurrentMonth())
+
+  useEffect(()=>{
+    setFilteredList(filterListByMonth(list, currentMonth))
+  }, [list, currentMonth])
+
+  return ( 
+    <C.Container>
+      <C.Header>
+        <C.HeaderText>
+          Sistema Financeiro
+        </C.HeaderText>
+      </C.Header>
+      <C.Body>
+        {/* Informações */}
+
+        {/* Inserção */}
+
+        <TableArea/>
+      </C.Body>
+    </C.Container>
+  )
 }
 
-export default App;
+export default App
---
+++ src/data/items.ts
@@ -0,0 +1,27 @@
+import {Item} from '../types/item'
+
+export const items: Item[] = [
+    { date: new Date(2021, 9, 15),
+    category: 'food',
+    title: 'McDonalds',
+    value: 32.12,
+    },
+
+    { date: new Date(2021, 9, 15),
+    category: 'food',
+    title: 'Burguer King',
+    value: 28.35,
+    },
+
+    { date: new Date(2021, 9, 16),
+    category: 'rent',
+    title: 'Aluguel Apt',
+    value: 32.12,
+    },
+
+    { date: new Date(2021, 10, 18),
+    category: 'salary',
+    title: 'Salário ACME',
+    value: 32.12,
+    },
+]
