706220d1efc3bfd78abe17b4343bff3b76b50c82
Luan Ferreira Campos <86481756+luanfcampos@users.noreply.github.com>
2021-10-07 00:57:11 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,30 @@ import { items } from './data/items'
 import { getCurrentMonth, filterListByMonth } from './helpers/dateFilter'
 import { TableArea } from './components/TableArea'
 import { InfoArea } from './components/InfoArea'
+
 const App = () => {
   const [list, setList] = useState(items)
-  const [filteredList, setFilteredList] = useState<Item[ ]>([])
+  const [filteredList, setFilteredList] = useState<Item[]>([])
   const [currentMonth, setCurrenteMonth] = useState(getCurrentMonth())
+  const [income, setIncome] = useState(0)
+  const [expense, setExpense] = useState(0)
 
   useEffect(()=>{
     setFilteredList(filterListByMonth(list, currentMonth))
   }, [list, currentMonth])
+
+  useEffect(()=>{
+    let incomeCount = 0
+    let expenseCount = 0
+
+    for(let i in filteredList){
+      categories[filteredList[i].category].expense ? expenseCount += filteredList[i].value : incomeCount += filteredList[i].value
+    }
+
+    setIncome(incomeCount)
+    setExpense(expenseCount)
+     
+  }, [filteredList])
 
   const handleMonthChange = (newMonth: string) => {
     setCurrenteMonth(newMonth)
@@ -31,6 +47,8 @@ const App = () => {
         <InfoArea
           currentMonth={currentMonth}
           onMonthChange={handleMonthChange}
+          income={income}
+          expense={expense}
           />
 
         {/* Inserção */}
